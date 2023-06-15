add767489f993324dd7f17bce94d64a1255b405e
3600-abhi <abhishekjaiswal3600@gmail.com>
2023-06-15 11:36:12 +0530

    added nodemailer inside index.js for demo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
+const mailSender = require('./config/email-config');
+
 const app = express();
 
 app.use(express.json());
@@ -9,6 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
   console.log(`Successfully started the server at PORT : ${ServerConfig.PORT}`);
+
+  const response = await mailSender.sendMail({
+    from: ServerConfig.GMAIL_ID,
+    to: 'abhishekjaiswal3600@gmail.com',
+    subject: 'Flight Ticket Details',
+    text: 'Congratulations Abhishek, You have successfully booked your Flight'
+  });
+
+  console.log("Mail Response = ", response);
 });
